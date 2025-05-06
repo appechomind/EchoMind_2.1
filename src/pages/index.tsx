2611b162fe143ee18821f2747ed8ddcfd0e62cff
appechomind <app.echo.mind@gmail.com>
2025-05-06 12:36:48 -0600

    Add: GitHub Pages deployment configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { EchoMind } from '../components/EchoMind/EchoMind';
+
+export default function Demo() {
+  return (
+    <div className="min-h-screen bg-slate-900">
+      <EchoMind />
+    </div>
+  );
+} 

e4e550d05af2470bc9d0a678a02a5f1b34154502
appechomind <app.echo.mind@gmail.com>
2025-05-06 14:18:08 -0600

    Update: Add vibrant animated blob background with gradient overlay

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
       },
       animation: {
         'gradient': 'gradient 15s ease infinite',
+        'blob': 'blob 7s infinite',
       },
       keyframes: {
         gradient: {
@@ -25,8 +26,34 @@ module.exports = {
             'background-position': 'right center'
           },
         },
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(30px, -50px) scale(1.1)',
+          },
+          '66%': {
+            transform: 'translate(-20px, 20px) scale(0.9)',
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.animation-delay-2000': {
+          'animation-delay': '2s',
+        },
+        '.animation-delay-4000': {
+          'animation-delay': '4s',
+        },
+      }
+      addUtilities(newUtilities)
+    }
+  ],
 } 

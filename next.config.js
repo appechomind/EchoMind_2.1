417d8988db70680f64086927515820c0a2f4195a
appechomind <app.echo.mind@gmail.com>
2025-05-06 00:12:32 -0600

    Configure for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/EchoMind_2.1',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
